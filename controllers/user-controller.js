c256ad7075f2d066d0d6d68539a27fbf80117bf8
JoeL <jlawton1203@gmail.com>
2020-09-26 10:07:39 -0500

    Added Update and Delete user routes and tested in Insomnia

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -35,6 +35,32 @@ const userController = {
             .catch(err => res.status(400).json(err));
     },
 
+    //update an existing user
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id:params.id}, body, {new:true, runValidators:true})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'No user found with that id.'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch (err => res.status(400).json(err));
+    },
+
+    //delete a user
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id:params.id})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'No user found with that id.'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+
 };
 
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,6 +4,8 @@ const {
     getAllUsers,
     getUserById,
     createUser,
+    updateUser,
+    deleteUser
 } = require('../../controllers/user-controller');
 
 //set up GET and POST routes for /api/users
@@ -17,8 +19,8 @@ router
 router
     .route('/:id')
     .get(getUserById)
-
-
+    .put(updateUser)
+    .delete(deleteUser)
 
 
 module.exports = router;
